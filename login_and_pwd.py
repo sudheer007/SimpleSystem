fac7a1e625026d7919019eb7fdfb0bf273fd45c2
Rahul Ramachandran <rramachnadra93@gmail.com>
2024-03-09 23:38:56 +0530

    first commit

--- login_and_pwd.py
+++ login_and_pwd.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
-from postgresDB_auth import authenticate_user
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from postgresDB_auth import authenticate_user, get_todo
 
 app =  Flask(__name__)
+
+app.secret_key = 'secret_key_0177#@!%$&'
 
 @app.route('/')
 def index():
@@ -14,10 +16,35 @@ def login():
 
     result = authenticate_user(username, password)
 
-    if result is not None:
-        return f"Successful Login. Welcom Mr.{username} "
+    if result:
+        #return f"Successful Login. Welcom Mr.{username} "
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+
     else:
         return "Incorrect Username or Password"
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' not in session:
+        return redirect(url_for('index'))
+    else:
+        return render_template('dashboard.html')
+    
+@app.route('/todos')
+def fetch_todo():
+    if 'username' not in session:
+        return jsonify({'error': 'user not logged in'}), 401
+    username = session['username']
+    result = get_todo(username)
+
+    return jsonify(result)
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
 
 if(__name__ == "__main__"):
     app.run(debug = True)
--- postgresDB_auth.py
+++ postgresDB_auth.py
@@ -41,7 +41,8 @@ def create_users_table():
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     Role VARCHAR(255) NOT NULL,
-    permissions VARCHAR(255) NOT NULL,
+    Public_TODO VARCHAR(255) NOT NULL,
+    Private_TODO VARCHAR(255) NOT NULL,
     hashed_password BYTEA NOT NULL,
     salt BYTEA NOT NULL
     );
@@ -62,8 +63,8 @@ def insert_user(username, role, permissions, password):
     print(hashed_password)
 
     insert_query = sql.SQL("""
-    INSERT INTO usersDB (username, role, permissions, hashed_password, salt) VALUES ({}, {}, {}, {}, {});
-    """).format(sql.Literal(username), sql.Literal(role), sql.Literal(permissions), sql.Literal(psycopg2.Binary(hashed_password)),  sql.Literal(psycopg2.Binary(salt)))
+    INSERT INTO usersDB (username, role, Public_TODO, Private_TODO, hashed_password, salt) VALUES ({}, {}, {}, {}, {}, {});
+    """).format(sql.Literal(username), sql.Literal(role), sql.Literal(permissions), sql.Literal(permissions), sql.Literal(psycopg2.Binary(hashed_password)),  sql.Literal(psycopg2.Binary(salt)))
     
     cursor.execute(insert_query)
     connection.commit()
@@ -84,16 +85,33 @@ def authenticate_user(ip_username, ip_password):
     stored_pwd = user_data[0]
     stored_salt = user_data[1]
     # print(stored_salt, type(stored_salt))
-    hashed_ip_pwd = bcrypt.hashpw(ip_password.encode('utf-8'),stored_salt.tobytes())
-    result = True if hashed_ip_pwd == stored_pwd else None
+    # hashed_ip_pwd = bcrypt.hashpw(ip_password.encode('utf-8'),stored_salt.tobytes())
+    result = bcrypt.checkpw(ip_password.encode('utf-8'), stored_pwd.tobytes())
+    # result = True if hashed_ip_pwd == stored_pwd else None
+    
 
-    print(hashed_ip_pwd, stored_pwd.tobytes())
-    print(stored_salt.tobytes())
+    # print(hashed_ip_pwd, stored_pwd.tobytes())
+    # print(stored_salt.tobytes())
 
     cursor.close()
     connection.close()
 
     return result
+def get_todo(username):
+    
+    connection = psycopg2.connect(**db_params)
+    cursor = connection.cursor()
+
+    public_query = "SELECT Public_TODO FROM usersDB WHERE username = %s"
+    private_query = "SELECT Private_TODO FROM usersDB WHERE username = %s"
+    
+    cursor.execute(public_query,(username,))
+    public_todos = cursor.fetchone()
+
+    cursor.execute(private_query,(username,))
+    private_todos = cursor.fetchone()
+    return {'public_todos': public_todos, 'private_todos': private_todos}
+
 
 # create_users_table()
 
